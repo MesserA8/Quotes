cc44a121f9e4b94f3f50f17f6b0163dfc71ddcfd
MesserA8 <atihon09@gmail.com>
2022-11-19 11:46:49 +0200

    background color changed

--- app/src/main/java/com/atihon09/quotes/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/atihon09/quotes/adapters/CategoryAdapter.kt
@@ -1,7 +1,5 @@
 package com.atihon09.quotes.adapters
 
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
